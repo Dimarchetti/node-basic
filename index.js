ea66fe97333ac77a937d7119922480686a35e2a7
Dimitri Marchetti <dimitri.marchetti@gmail.com>
2020-03-21 02:33:28 -0300

    api com requisicoes get

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const server = express()
 
-server.get('/teste', (req, res) => {
-    return res.json({message: 'Hello World!'})
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "Dimitri", "Email": "dimitri.marchetti@gmail.com" }
+
+// CRUD - Create, Read, Update, Delete
+
+const users = ['Dimitri', 'Andréia', 'Luiz' ]
+
+server.get('/users', (req, res) => {
+    return res.json(users)
+})
+
+server.get('/user/:index', (req, res) => {
+    //const { nome } = req.query // Pega as query params na url
+    //const { id } = req.params // Pega o id passado na requisição
+    const { index } = req.params // Pega o id passado na requisição
+
+    return res.json(users[index])
 })
 
 server.listen(3000)
