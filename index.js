a670d623a2413beb70addcb441d00adad97f477c
Dimitri Marchetti <dimitri.marchetti@gmail.com>
2020-03-21 03:03:52 -0300

    api com CRUD feito

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "name": "Dimitri", "Email": "dimitri.marchetti@gmail.com" }
@@ -20,6 +22,33 @@ server.get('/user/:index', (req, res) => {
     const { index } = req.params // Pega o id passado na requisição
 
     return res.json(users[index])
+})
+
+
+server.post('/users', (req, res) => {
+    const { name } = req.body // Pega o nome de dentro do Body da requisição
+
+    users.push(name) // Coloca o nome pego dentro do Array
+
+    return res.json(users) // Mostra o array já com o usuário novo
+})
+
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params // Pega o index passado na requisição
+    const { name } = req.body // Pega o nome no body da requisição
+
+    users[index] = name
+
+    return res.json(users)
+})
+
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params // Pega o index passado na requisição
+
+    users.splice(index, 1) // Encontra o index dentro do array e apaga uma posição
+
+    // return res.json(users) // retorna lista de usuários após deletar
+    return res.send() //Não retorna nenhum conteúdo após deletar
 })
 
 server.listen(3000)
